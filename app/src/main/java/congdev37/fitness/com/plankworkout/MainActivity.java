a3bec8dc08f463363fbde157c28c1288d575e16a
congcau37 <trandinhcong23081997@gmail.com>
2019-02-14 10:34:05 +0700

    first commit
    home

---
+++ app/src/main/java/congdev37/fitness/com/plankworkout/MainActivity.java
@@ -0,0 +1,13 @@
+package congdev37.fitness.com.plankworkout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
